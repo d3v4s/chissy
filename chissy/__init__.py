2ed84562b88d258a761c30596b2315febf15c7df
d3v4s <devas@devas>
2019-12-18 18:13:50 +0100

    add devas site link in author

--- chissy/__init__.py
+++ chissy/__init__.py
@@ -1,6 +1,6 @@
 
 __version_info__ = ('0', '8', 'beta', '1')
 __version__ = '.'.join(__version_info__)
-__author__ = 'Andrea Serra (DevAS) https://github.com/d3v4s'
+__author__ = 'Andrea Serra (DevAS) https://devas.info - https://github.com/d3v4s'
 
 __all__ = ['core', 'model']
